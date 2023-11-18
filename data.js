8e81feb48ecdfda4d37708732d4d50d00c03fe9b
bethmannRolf <devak.robe@gmail.com>
2023-11-18 12:25:07 +0100

    second commit

---
+++ data.js
@@ -0,0 +1,201 @@
+/*
+
+formattedPrice
+
+ const formattedPrice =dish['price'].toFixed(2).replace('.', ',');
+
+*/
+let dishes = [
+
+   // favorite dishes
+   {
+      "amount": 1,
+      "dishnumber": "1",
+      "dishName": "Currywurst mit Pommes",
+      "dishdescription": "Mit hausgemachter Sauce und Pommes Frites",
+      "price": 6.99,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "2",
+      "dishName": "Flammkuchen",
+      "dishdescription": "Klassisch mit Speck und Zwiebeln",
+      "price": 10.60,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "3",
+      "dishName": "Würzfleisch",
+      "dishdescription": "Mit Käse überbacken und Brot",
+      "price": 5.79,
+      "comment" : ""
+   },
+
+
+   // steak dishes
+
+   {
+      "amount": 1,
+      "dishnumber": "4",
+      "dishName": "Putensteak mit Senfmarinade",
+      "dishdescription": "Mit Salatbeilage und Backkartoffel",
+      "price": 20.24,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "5",
+      "dishName": "Rinderhüftsteak",
+      "dishdescription": "Mit Salatbeilage und Pommes Frites",
+      "price": 25.45,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "6",
+      "dishName": "Rumpsteak 200g",
+      "dishdescription": "Kerniges Fleisch vom Rinderrücken mit Ofenkartoffel und Salat",
+      "price": 30.62,
+      "comment" : ""
+   },
+
+
+   //burger dishes
+
+
+   {
+      "amount": 1,
+      "dishnumber": "7",
+      "dishName": "Cheeseburger",
+      "dishdescription": "Mit Salatbeilage und Pommes Frites",
+      "price": 15.46,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "8",
+      "dishName": "Hamburger",
+      "dishdescription": "Mit Salatbeilage und Pommes Frites",
+      "price": 10.67,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "9",
+      "dishName": "Chrispy Chicken Burger",
+      "dishdescription": "Mit Salatbeilage und Pommes Frites",
+      "price": 15.87,
+      "comment" : ""
+   },
+
+
+   // schnitzel dishes
+
+   {
+      "amount": 1,
+      "dishnumber": "10",
+      "dishName": "Jägerschnitzel",
+      "dishdescription": "Schweineschnitzel mit Champignons und Pommes Frites ",
+      "price": 16.20,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "11",
+      "dishName": "Hamburger Schnitzel",
+      "dishdescription": "Schweineschnitzel mit Spiegelei und Bratkartoffeln",
+      "price": 15.89,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "12",
+      "dishName": "Schnitzel Wiener Art",
+      "dishdescription": "Schweineschnitzel mit Pommes",
+      "price": 10.54,
+      "comment" : ""
+   },
+
+
+
+   // salad dishes
+
+   {
+      "amount": 1,
+      "dishnumber": "13",
+      "dishName": "Fischersalat",
+      "dishdescription": "Mitit gebratenem Zanderfilet",
+      "price": 15.23,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "14",
+      "dishName": "Mälzersalat",
+      "dishdescription": "Mit marinierten Putenbruststreifen",
+      "price": 10.89,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "15",
+      "dishName": "Caesar Salat",
+      "dishdescription": "Mit Croutons und Parmesan",
+      "price": 5.78,
+      "comment" : ""
+   },
+
+   // drinks dishes
+
+   {
+      "amount": 1,
+      "dishnumber": "16",
+      "dishName": "Dunkles Bier",
+      "dishdescription": "Vollmundig vom Fass",
+      "price": 3.21,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "17",
+      "dishName": "Mineralwasser",
+      "dishdescription": "Mit oder ohne Sprudel",
+      "price": 1.73,
+      "comment" : ""
+   },
+
+   {
+      "amount": 1,
+      "dishnumber": "18",
+      "dishName": "Wein",
+      "dishdescription": "Rot, Weiß oder Rosé",
+      "price": 6.99,
+      "comment" : ""
+   }
+
+
+
+
+
+
+];
+
+
+
+
+let dishesBasket = [
+
+];
---
+++ script.js
@@ -0,0 +1,470 @@
+HEART_LIKED = false;
+DELIVERY_COSTS = 2.00;
+
+
+
+function renderDishes() {
+    hideSearchInput();
+    renderLike();
+    renderFavoriteDishes();
+    renderSteakDishes();
+    renderBurgerDishes();
+    renderSchnitzelDishes();
+    renderSaladDishes();
+    renderDrinkDishes();
+    loadBasketFromLS();
+    renderBasket();
+    hideClearedBasket();
+    hideInformation()
+}   
+
+function hideInformation(){
+    document.getElementById('information-popup').classList.add('d-none')
+}
+
+// Gerichte rendern
+
+function openPopup(){
+
+    document.getElementById('information-popup').classList.toggle('d-none')
+}
+
+
+
+function filterDishes(){
+
+   let search = document.getElementById('search-input').value
+    search = search.toLowerCase();
+
+
+    let searchedMenuContainer = document.getElementById('menu-card');
+    searchedMenuContainer.innerHTML = '';
+
+    for (let i = 0; i < dishes.length; i++) {
+        let searchedDish = dishes[i];
+
+
+        if(searchedDish['dishName'].toLowerCase().includes(search)){
+
+            searchedMenuContainer.innerHTML += `
+            <div class="single-dish">
+               <div class="single-dish-headline">
+                  <div class="single-dish-headline-firstpart">
+                      <h3 class="heading-dish">${searchedDish['dishName']}</h3>
+                      <img src="./img/info.png" class="single-dish-info" id="addingDishes${i}">
+                  </div>
+                  <img src="./img/add.png" class="add-button" onclick="addToBasket(${i})" >
+               </div>
+               <span>${searchedDish['dishdescription']}</span>
+               <span>${searchedDish['price']}€</span>
+            </div>`
+        }
+    }
+}
+
+function hideSearchInput(){
+
+    document.getElementById('search-and-close').classList.add('d-none');
+
+}
+
+function hideXSearchInput(){
+
+    document.getElementById('search-and-close').classList.add('d-none');
+}
+
+
+function showInputSearch(){
+    document.getElementById('search-and-close').classList.remove('d-none')
+
+ }
+
+function renderFavoriteDishes() {
+    let dishesContent = document.getElementById('favorites-div');
+    dishesContent.innerHTML = '';
+
+    for (let i = 0; i < 3; i++) {
+        const dish = dishes[i];
+        const formattedPrice =dish['price'].toFixed(2).replace('.', ',');
+        dishesContent.innerHTML += ` 
+    <div class="single-dish">
+       <div class="single-dish-headline">
+          <div class="single-dish-headline-firstpart">
+              <h3 class="heading-dish">${dish['dishName']}</h3>
+              <img src="./img/info.png" class="single-dish-info" id="addingDishes${i}">
+          </div>
+          <img src="./img/add.png" class="add-button" onclick="addToBasket(${i})" >
+       </div>
+       <span>${dish['dishdescription']}</span>
+       <span>${formattedPrice}€</span>
+    </div>`
+    }
+}
+
+function renderSteakDishes() {
+
+    let dishesContent = document.getElementById('steak-div');
+    dishesContent.innerHTML = '';
+
+    for (let i = 3; i < 6; i++) {
+        const dish = dishes[i];
+        const formattedPrice =dish['price'].toFixed(2).replace('.', ',');
+        dishesContent.innerHTML += ` 
+    <div class="single-dish">
+       <div class="single-dish-headline">
+          <div class="single-dish-headline-firstpart">
+              <h3 class="heading-dish">${dish['dishName']}</h3>
+              <img src="./img/info.png" class="single-dish-info">
+          </div>
+          <img src="./img/add.png" class="add-button" onclick="addToBasket(${i})" >
+       </div>
+       <span>${dish['dishdescription']}</span>
+       <span>${formattedPrice}€</span>
+    </div>`
+    }
+}
+
+function renderBurgerDishes() {
+
+    let dishesContent = document.getElementById('burger-div');
+    dishesContent.innerHTML = '';
+
+    for (let i = 6; i < 9; i++) {
+        const dish = dishes[i];
+        const formattedPrice =dish['price'].toFixed(2).replace('.', ',');
+        dishesContent.innerHTML += ` 
+        <div class="single-dish">
+           <div class="single-dish-headline">
+              <div class="single-dish-headline-firstpart">
+                  <h3 class="heading-dish">${dish['dishName']}</h3>
+                  <img src="./img/info.png" class="single-dish-info">
+              </div>
+              <img src="./img/add.png" class="add-button" onclick="addToBasket(${i})">
+           </div>
+           <span>${dish['dishdescription']}</span>
+           <span>${formattedPrice}€</span>
+        </div>`
+    }
+}
+
+function renderSchnitzelDishes() {
+
+    let dishesContent = document.getElementById('schnitzel-div');
+    dishesContent.innerHTML = '';
+
+    for (let i = 9; i < 12; i++) {
+        const dish = dishes[i];
+        const formattedPrice =dish['price'].toFixed(2).replace('.', ',');
+        dishesContent.innerHTML += ` 
+        <div class="single-dish">
+           <div class="single-dish-headline">
+              <div class="single-dish-headline-firstpart">
+                  <h3 class="heading-dish">${dish['dishName']}</h3>
+                  <img src="./img/info.png" class="single-dish-info">
+              </div>
+              <img src="./img/add.png" class="add-button" onclick="addToBasket(${i})" >
+           </div>
+           <span>${dish['dishdescription']}</span>
+           <span>${formattedPrice}€</span>
+        </div>`
+    }
+}
+// dish['price']
+function renderSaladDishes() {
+    let dishesContent = document.getElementById('salad-div');
+    dishesContent.innerHTML = '';
+    for (let i = 12; i < 15; i++) {
+        const dish = dishes[i];
+        const formattedPrice =dish['price'].toFixed(2).replace('.', ',');
+        dishesContent.innerHTML += ` 
+        <div class="single-dish">
+           <div class="single-dish-headline">
+              <div class="single-dish-headline-firstpart">
+                  <h3 class="heading-dish">${dish['dishName']}</h3>
+                  <img src="./img/info.png" class="single-dish-info">
+              </div>
+              <img src="./img/add.png" class="add-button" onclick="addToBasket(${i})">
+           </div>
+           <span>${dish['dishdescription']}</span>
+           <span>${formattedPrice}€</span>
+        </div>`
+    }
+}
+
+function renderDrinkDishes() {
+    let dishesContent = document.getElementById('drinks-div');
+    dishesContent.innerHTML = '';
+    for (let i = 15; i < 18; i++) {
+        const dish = dishes[i];
+        const formattedPrice =dish['price'].toFixed(2).replace('.', ',');
+        dishesContent.innerHTML += ` 
+        <div class="single-dish">
+           <div class="single-dish-headline">
+              <div class="single-dish-headline-firstpart">
+                  <h3 class="heading-dish">${dish['dishName']}</h3>
+                  <img src="./img/info.png" class="single-dish-info">
+              </div>
+              <img src="./img/add.png" class="add-button" onclick="addToBasket(${i})">
+           </div>
+           <span>${dish['dishdescription']}</span>
+           <span>${formattedPrice}€</span>
+        </div>`
+    }
+}
+
+
+function hideClearedBasket(){
+
+    document.getElementById('checkbox').classList.add('d-none');
+}
+
+function clearBasket(){
+
+
+
+let clearedContentBasket = document.getElementById('content-basket');
+clearedContentBasket.innerHTML = '';
+
+let clearedBasketEndcalculations = document.getElementById('basket-endcalculations')
+clearedBasketEndcalculations.innerHTML ='';
+
+
+document.getElementById('checkbox').classList.remove('d-none');
+
+}
+
+
+function renderLike() {
+    let likeButton = document.getElementById('like-section');
+    likeButton.innerHTML = '';
+    if (HEART_LIKED == false) {
+        likeButton.innerHTML =
+            `
+        <img id="like-img" src="./img/favorite.png" alt="Favoriten" onclick="changeLikeColor()">
+         `
+    }
+    else {
+        likeButton.innerHTML = `
+        <img id="like-img" src="./img/favorite_true.png" alt="Favoriten" onclick="changeLikeColor()">
+        `
+    }
+}
+
+function changeLikeColor() {
+    if (HEART_LIKED == false) {
+        HEART_LIKED = true;
+    }
+    else {
+        HEART_LIKED = false;
+    }
+    renderLike();
+}
+
+
+
+function addToBasket(index) {
+
+    const existingIndex = dishesBasket.findIndex(item => item.dishnumber === dishes[index].dishnumber);
+    if (existingIndex !== -1) {
+        dishesBasket[existingIndex].amount += 1;
+    } else {
+        const selectedDish = { ...dishes[index], amount: 1 };
+        dishesBasket.push(selectedDish);
+    }
+    renderBasket();
+    saveBasketToLS();
+}
+
+
+
+function renderBasket() {
+    document.getElementById('emty-basket').classList.add('d-none');
+    let basketContent = document.getElementById('content-basket');
+    basketContent.innerHTML = '';
+    for (let i = 0; i < dishesBasket.length; i++) {
+        const basketdish = dishesBasket[i];
+        let newSum = basketdish['price'] * basketdish['amount'];
+
+
+        basketContent.innerHTML += `
+<div class="singledish-container">
+    <div class="firstline-dishbasket">
+        <span class="amount-basket">${basketdish['amount']}</span>
+        <span class="dishname-basket">${basketdish['dishName']}</span>
+        <span class="sum-dishbasket">${newSum.toFixed(2)}€</span>
+    </div>
+    <div class="secondline-dishbasket">
+        <span onclick="openInput(${i})" class="make-annotation">Anmerkung hinzufügen</span>
+        <div class="change-amounts">
+            <img class="basketDishRemove" onclick="reduceAmount(${i})" src="./img/remove.png" width="80%" height="auto">
+            <span class="amount-basket">${basketdish['amount']}</span>
+            <img class="basketDishAdd" onclick="increaseAmount(${i})" src="./img/add.png" width="80%" height="auto">
+        </div>
+    </div>
+
+    <div id="input-container${i}"></div>
+    <div id="saved-comment">${basketdish['comment']}</div>
+</div>
+`
+    }
+    chechIfBill()
+    
+}
+
+
+function chechIfBill() {
+
+    let endCalc = document.getElementById('basket-endcalculations')
+
+    if (dishesBasket == '') {
+
+        endCalc.innerHTML = '';
+
+    } else {
+
+        renderBill()
+    }
+
+}
+
+
+
+
+
+
+function openInput(i) {
+    let inputContainer = document.getElementById(`input-container${i}`);
+    inputContainer.innerHTML = '';
+
+    inputContainer.innerHTML = `
+        <div >
+            <input id="annotation-input${i}" type="text" placeholder="Anmerkung hinzufügen . . .">
+            <button onclick="saveInput(${i})">Speichern</button>
+        </div>
+    `;
+}
+
+
+function saveInput(i) {
+
+    let annotationInput = document.getElementById(`annotation-input${i}`);
+    let annotation = annotationInput.value;
+    dishesBasket[i]['comment'] = annotation;
+    renderBasket();
+}
+
+
+
+function reduceAmount(i) {
+
+    if (dishesBasket[i].amount > 1) {
+        dishesBasket[i].amount -= 1;
+    } else {
+        dishesBasket.splice(i, 1);
+    }
+
+    renderBasket();
+    saveBasketToLS();
+
+
+}
+
+function increaseAmount(i) {
+
+    dishesBasket[i].amount += 1;
+    renderBasket();
+    saveBasketToLS();
+}
+
+
+function calculateBill() {
+
+    let subtotal = 0;
+
+    //nun die Zwischensumme
+
+    for (let i = 0; i < dishesBasket.length; i++) {
+        const basketdish = dishesBasket[i];
+
+        subtotal += basketdish['price'] * basketdish['amount'];
+    }
+
+    return {
+        subtotal
+    }
+
+
+}
+
+
+
+function renderBill() {
+
+    let billing = document.getElementById('basket-endcalculations');
+    billing.innerHTML = '';
+
+
+    let { subtotal } = calculateBill();
+
+    let total = subtotal + DELIVERY_COSTS;
+
+    billing.innerHTML = `
+    <div id=""whole-bill> 
+    <div id="billing-description">
+         <div id="subtotal">
+         <span>Zwischensumme</span>
+         <span>${subtotal.toFixed(2)} €</span>
+         </div>
+         <div id="delivery-costs">
+         <span>Lieferkosten</span>
+         <span>${DELIVERY_COSTS.toFixed(2)} €</span>
+         </div>
+        <div id="total-sum">
+        <span>Gesamt</span>
+        <span>${total.toFixed(2)} €</span>
+        </div>
+    
+    </div>
+  
+    <button class="pay-class" id="paybutton" onclick="clearBasket()">
+    <span  id="paybutton-span">Bezahlen (${total.toFixed(2)} €)</span>
+    </button>
+    </div>
+    
+    
+    `
+}
+
+
+ function additionalOrder(){
+
+    dishesBasket = [];
+    saveBasketToLS();
+    hideClearedBasket()
+    renderBasket()
+
+
+ }
+
+
+
+function increaseAmount(i) {
+
+    dishesBasket[i].amount += 1;
+    renderBasket();
+    saveBasketToLS();
+}
+
+function saveBasketToLS() {
+    let dishesBasketAsText = JSON.stringify(dishesBasket);
+    localStorage.setItem('dishesBasket', dishesBasketAsText);
+}
+
+function loadBasketFromLS() {
+    let dishesBasketAsText = localStorage.getItem('dishesBasket');
+    if (dishesBasketAsText) {
+        dishesBasket = JSON.parse(dishesBasketAsText);
+    }
+}
+
+
